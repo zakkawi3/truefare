e81b6272e53e60191d7b5a81de874ca07a82c555
Majd Khawaldeh <majd@gatech.edu>
2024-11-17 17:15:25 -0500

    small change

--- app/hooks/usePaymentModal.tsx
+++ app/hooks/usePaymentModal.tsx
@@ -7,7 +7,7 @@ interface PaymentModalStore {
   }
   
   const usePaymentModal = create<PaymentModalStore>((set) => ({
-    isOpen: true,
+    isOpen: false,
   
     onOpen: () => set({ isOpen: true }),
     onClose: () => set({ isOpen: false }),
